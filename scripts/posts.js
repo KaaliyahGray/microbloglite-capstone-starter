ba3ad165dfe6a12576ab15be15bd7cbf3fb46453
KaaliyahGray <166437615+KaaliyahGray@users.noreply.github.com>
2024-06-24 18:41:13 -0700

    update post.index
    update css

--- scripts/posts.js
+++ scripts/posts.js
@@ -1,5 +1,3 @@
-
-
 "use strict";
 
 window.onload= function (){getPost()};
@@ -71,28 +69,59 @@ function createPost() {
 
 }
 
+// function renderPost(postData) {
+//    // console.log(postData);
+// let postArray = [];
+//     const loginData = getLoginData();
+//     const theposts = document.getElementById("posts");
+//     postArray.push(postData);
+//   //  console.log(postArray);
+//     postData.forEach(data => {
+//         console.log(data.text)
+//     const postElement = document.createElement("posts");
+//     postElement.classList.add("post");
+//     postElement.innerHTML = `
+//     <div class="card-body">
+//         <h2>Post ID: ${data.username}</h2>
+//         <p>${data.text}</p>
+//         <hr>
+//         </div>
+//     `;
+
+//     theposts.appendChild(postElement)
+//     ;
+// });
+
+// }
+
 function renderPost(postData) {
-   // console.log(postData);
-let postArray = [];
-    const loginData = getLoginData();
     const theposts = document.getElementById("posts");
-    postArray.push(postData);
-  //  console.log(postArray);
+
     postData.forEach(data => {
-        console.log(data.text)
-    const postElement = document.createElement("posts");
-    postElement.classList.add("post");
-    postElement.innerHTML = `
-        <h2>Post ID: ${data.username}</h2>
-        <p>${data.text}</p>
-        <hr>
-    `;
+        const postCard = document.createElement("div");
+        postCard.classList.add("card", "mb-3");
+        postCard.style.maxWidth = "540px";
 
-    theposts.appendChild(postElement)
-    ;
-});
+        postCard.innerHTML = `
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="..." class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-9">
+                    <div class="card-body">
+                    <h5 class="card-title">${data.username}</h5>
+                    <p class="card-text">${data.text}</p>
+                    <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                    <button class="btn btn-primary like-button" data-post-id="${data.postId}">Like</button>
+                </div>
+            </div>
+        </div>
+        `;
 
+        theposts.appendChild(postCard);
+    });
 }
+
 
 
 
@@ -118,4 +147,33 @@ fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts?limit=300&o
 function getLoginData () {
     const loginJSON = window.localStorage.getItem("login-data");
     return JSON.parse(loginJSON) || {};
+}
+
+
+
+
+// like a post 
+
+function likeaPost(){
+    const myHeaders = new Headers();
+    myHeaders.append("accept", "application/json");
+    myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlBoYXJlbGwgV2lsbGlhbXMiLCJpYXQiOjE3MTkyNzAwMjUsImV4cCI6MTcxOTM1NjQyNX0.HvuhVePwjXVgth2LYgHAQ77zA-DAWI9jU7s3hXlazjk");
+    myHeaders.append("Content-Type", "application/json");
+    
+    const raw = JSON.stringify({
+      "postId": "string"
+    });
+    
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow"
+    };
+    
+    fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/likes", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.error(error));
+
 }
